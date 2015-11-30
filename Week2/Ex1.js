107a9bdcb421b8c57c78cf7ceb9f679742145744
jhn316 <nissanka_w@yahoo.com>
2015-11-30 15:12:43 +0200

    commit week2 work

---
+++ Week2/Ex1.js
@@ -0,0 +1,5 @@
+var todo=["start company", "round B funding", "launch IPO", 
+          "round A funding", "build app", "employ staff"];  
+var priority=[3, 5, 6, 4, 2, 1];
+alert(todo);
+alert(priority);
